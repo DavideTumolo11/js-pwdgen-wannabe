c84d94289f55930c07e652d5e1c6aed0f3e68402
DavideTumolo11 <davidetumolo11@gmail.com>
2023-11-18 18:28:09 +0100

    error

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+ - prompt
+ - const/let
+ - backticks `${}`
+ - 
+ */
+
+
+//  chiedi all'utente come si chiama e lo salvi in una variabile
+const user_name = prompt('Come ti chiami?');
+
+// chiedi all'utente il suo cognome
+const user_lastname = prompt('Quale è il tuo cognome?');
+
+// chiedi all'utenete qualeè il suo colore preferito
+const user_fav_color = prompt('Quale è il tuo colore preferito?');
+
+// CONSOLE LOG
+console.log(user_name, user_lastname, user_fav_color + 23);
+
+console.log(password);
+
+// Scrivi sulla pagina la password generata
+document.getElementById('password').innerHTML = password;
+
+
+
